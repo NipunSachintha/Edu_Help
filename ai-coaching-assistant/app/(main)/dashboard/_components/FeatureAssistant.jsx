b60eb8cd06f5b63dcf8c8ae410f0f4eb664857cd
NS <157737507+NipunSachintha@users.noreply.github.com>
2025-07-06 12:27:50 +0530

    Add feature assistant UI and discussion room creation
    Introduces new dashboard components for feature selection, user input, history, and feedback. Implements dialog and textarea UI components, a blur-fade animation, and integrates expert and topic selection for creating discussion rooms in Convex. Updates schema and API to support discussion rooms, and adds related assets and dependencies.

--- ai-coaching-assistant/app/(main)/dashboard/_components/FeatureAssistant.jsx
+++ ai-coaching-assistant/app/(main)/dashboard/_components/FeatureAssistant.jsx
@@ -1,7 +1,11 @@
 "use client"
 import { Button } from '@/components/ui/button';
+import { ExpertsList } from '@/services/Options';
 import { useUser } from '@stackframe/stack'
+import Image from 'next/image';
 import React from 'react'
+import { BlurFade } from '@/components/magicui/blur-fade';
+import UserInputDialog from './UserInputDialog';
 
 function FeatureAssistant() {
     const user = useUser() ;
@@ -13,6 +17,22 @@ function FeatureAssistant() {
                     <h2 className='text-3xl font-bold'>Welcome back, {user ?. displayName}</h2>
                 </div>
                 <Button>Profile</Button>
+                
+            </div>
+
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-10 mt-4'>
+                {ExpertsList.map((option, index) => (
+                    <BlurFade key={option.icon} delay={0.25 + index * 0.05} inView>
+                         <div key = {index} className='p-3 bg-secondary rounded-3xl flex flex-col items-center justify-center gap-10 hover:bg-secondary/80 transition-all cursor-pointer mt-10'>
+                            <UserInputDialog ExpertsList={option}>
+                            <div key = {index} className='flex flex-col items-center justify-center cursor-pointer'>
+                                <Image src={option.icon} alt={option.name} width={200} height={120} className='h-[70px] w-[90px] mt-2 hover:rotate-8 transition-all' />
+                                <h2 className='mt-0'>{option.name}</h2>
+                            </div>
+                        </UserInputDialog>
+                        </div>
+                   </BlurFade>
+                ))}
             </div>
         </div>
     )
---
+++ ai-coaching-assistant/app/(main)/dashboard/_components/History.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function History() {
+    return (
+        <div>
+            <h2 className='font-bold text-xl'>Your Prevoius Lectures</h2>
+
+            <h2 className='text-gray-400'>you dont have anything</h2>
+        </div>
+    )
+}
+
+export default History
